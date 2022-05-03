52295987ace2008331634fa53f9a081c0a712b42
c-viniciussantos <c.viniciussantos@outlook.com>
2022-05-02 21:44:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -540,7 +540,7 @@ def get_stats(github):
     if show_updated_date.lower() in truthy:
         now = datetime.datetime.utcnow()
         d1 = now.strftime(updated_date_format) - timedelta(hours=3)
-        stats = stats + "\n Ultima atualização em " + d1 + " GMT-3"
+        stats = stats + "\n Última atualização em " + d1 + " GMT-3"
 
     return stats
 
